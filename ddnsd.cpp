405e8f1f0dad34fb7af23fe52925d49680e7d7b8
Schmorzel <tizimmermann01@gmail.com>
2018-01-17 15:23:11 +0100

    added option to disable update checker

--- ddnsd.cpp
+++ ddnsd.cpp
@@ -80,22 +80,24 @@ std::string read_config(std::string file_path, std::string config_key) {
 
 int main(int argc, char** argv) {
 	std::fstream f;
-	std::string version = "v4.5.1";
-	std::string release_date = "06.01.2018";
+	std::string version = "v4.5.2";
+	std::string release_date = "17.01.2018";
 	std::string config = "/etc/ddns/ddnsd.conf";
-        std::string remote_version = shell_exec("curl --silent https://raw.githubusercontent.com/Schmorzel/ddnsd/master/.version");
-        boost::replace_all(remote_version, "\n", "");
-        boost::replace_all(remote_version, "\r", "");
-        if (remote_version != version) {
-		if (remote_version.length() == 0) {
-			std::cout << "Could not check for updates!" << std::endl;
-		}
-		else {
-                	std::cout << "Update to version " << remote_version << " available!" << std::endl;
-                	std::cout << "To update clone the following Git Repository: https://github.com/Schmorzel/ddnsd" << std::endl;
-		}
-        }
-
+	std::string update_checker = read_config(config, "update_checker = ");
+	if (update_checker == "true") {
+		std::string remote_version = shell_exec("curl --silent https://raw.githubusercontent.com/Schmorzel/ddnsd/master/.version");
+     	   	boost::replace_all(remote_version, "\n", "");
+        	boost::replace_all(remote_version, "\r", "");
+        	if (remote_version != version) {
+			if (remote_version.length() == 0) {
+				std::cout << "Could not check for updates!" << std::endl;
+			}
+			else {
+                		std::cout << "Update to version " << remote_version << " available!" << std::endl;
+                		std::cout << "To update clone the following Git Repository: https://github.com/Schmorzel/ddnsd" << std::endl;
+			}
+        	}
+	}
 	if (argc > 1) {
 		if (std::string(argv[1]) == "-version" || std::string(argv[1]) == "--version") {
 			std::cout << "DDNSD " << version << " " << release_date << std::endl;
@@ -105,8 +107,8 @@ int main(int argc, char** argv) {
 	//Read config
 	std::string enabled;
 	enabled = read_config(config, "enabled = ");
-	if (enabled == "0") {
-		std::cout << "/etc/ddns/ddnsd.conf: enabled is set to 0... Stopping service..." << std::endl;
+	if (enabled == "false") {
+		std::cout << "/etc/ddns/ddnsd.conf: enabled is set to false... Stopping service..." << std::endl;
 		exit(0);
 	}
 	std::string update_freq_string;
@@ -128,7 +130,7 @@ int main(int argc, char** argv) {
 	//Check if config exists, if not create config files
 	if (config_version.length() == 0) {
 		std::cout << "It looks like the service is started first time, creating configuration files..." << std::endl;
-		system("mkdir -p /etc/ddns & echo \"#DDNSD Configuration\n\n#Enable (1)/Disable (0) the service:\nenabled = 1\n\n#IP-Adress update frequency:\nupdate_freq = 60\n\n#DNS Zone name (used for getting zone serial and update message):\nzone_name = Example.com\n\n#Path to DNS zone file:\nzone_path = /etc/bind/db.example.com\n\n#Commands that will be executed after DNS zone update (seperated by &):\npost_update_cmds = service bind9 restart & custom_cmd & custom_cmd2\n\n#Do not touch:\nconfig_version = 1\n\" >/etc/ddns/ddnsd.conf");
+		system("mkdir -p /etc/ddns & echo \"#DDNSD Configuration\n\n#Enable (true)/Disable (false) the service:\nenabled = true\n\n#IP-Adress update frequency:\nupdate_freq = 60\n\n#DNS Zone name (used for getting zone serial and update message):\nzone_name = Example.com\n\n#Path to DNS zone file:\nzone_path = /etc/bind/db.example.com\n\n#Commands that will be executed after DNS zone update (seperated by &):\npost_update_cmds = service bind9 restart & custom_cmd & custom_cmd2\n\n#Enable (true)/Disable (false) the Update Checker\nupdate_checker = true\n\n#Do not touch:\nconfig_version = 1\n\" >/etc/ddns/ddnsd.conf");
 		system("curl --silent https://v4.ident.me >/etc/ddns/.oldip.ddns");
 		std::cout << "Config file created." << std::endl;
 		std::cout << "Please edit /etc/ddns/ddnsd.conf" << std::endl;;

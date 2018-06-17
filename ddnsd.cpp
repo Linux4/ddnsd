270235cc27d591da5b10fcb2328deb240e4d6644
Schmorzel <tizimmermann01@gmail.com>
2018-06-17 13:14:47 +0200

    added header files

--- ddnsd.cpp
+++ ddnsd.cpp
@@ -1,231 +1,13 @@
-#include <iostream>
-#include <stdlib.h>
-#include <fstream>
-#include <chrono>
-#include <thread>
-#include <arpa/inet.h>
-#include <locale>
-#include <string>
-#include <ctime>
-#include <sstream>
-#include <unistd.h>
-#include <boost/algorithm/string/replace.hpp>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <array>
-#include <cstdio>
-#include <memory>
-#include <stdexcept>
-#include <boost/algorithm/string.hpp>
-#include <vector>
-
-bool is_ipv4_address(const std::string& str) {
-	struct sockaddr_in sa;
-	return inet_pton(AF_INET, str.c_str(), &(sa.sin_addr)) != 0;
-}
-
-bool is_ipv6_address(const std::string& str) {
-	struct sockaddr_in6 sa;
-	return inet_pton(AF_INET6, str.c_str(), &(sa.sin6_addr)) != 0;
-}
-
-struct Time {
-	Time( std::time_t tm, const std::string format )
-	: m_tm( tm ), m_format( format )
-	{}
-	friend std::ostream& operator<<( std::ostream& out, const Time& t ) {
-		typedef std::ostreambuf_iterator< char > out_type;
-		typedef std::time_put< char, out_type > time_put_facet_type;
-		const time_put_facet_type& fac = std::use_facet< time_put_facet_type >( out.getloc() );
-		const char* pattern = t.m_format.c_str();
-		fac.put( out_type( out ), out, out.fill(), std::localtime( &t.m_tm ), pattern, pattern + t.m_format.size() );
-		return out;
-	}
-	private:
-		std::time_t m_tm;
-		std::string m_format;
-};
-
-inline bool file_exists(const std::string& name) {
-	struct stat buffer;
-	return (stat (name.c_str(), &buffer) == 0);
-}
-
-std::string shell_exec(std::string cmd) {
-	std::array<char, 1024> buffer;
-	std::string result;
-	std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
-	if (!pipe) {
-		std::cerr << "ERROR executing " << cmd << std::endl;
-		exit(1);
-	}
-	while (!feof(pipe.get())) {
-		if (fgets(buffer.data(), 1024, pipe.get()) != nullptr) {
-			result += buffer.data();
-		}
-	}
-	return result;
-}
-
-std::string read_config(std::string file_path, std::string config_key) {
-        std::fstream f;
-        std::string result;
-        std::string config_value;
-        f.open(file_path, std::fstream::in);
-        int length = config_key.length();
-        while(getline(f, config_value)) {
-                if(config_value.substr(0, length) == config_key) {
-                        boost::replace_all(config_value, config_key, "");
-                        result = config_value;
-                        break;
-                }
-        }
-        f.close();
-        return result;
-}
-
-void updateip(std::string zone, std::string OLDIP, std::string IP, bool ipv6) {
-	std::vector<std::string> zone_name_path;
-	boost::split(zone_name_path, zone, boost::is_any_of(":"));
-	int zonesize = zone_name_path.size();
-	std::string zone_path;
-	std::string zone_name;
-	if(zonesize == 2) {
-		zone_name = zone_name_path.at(0);
-		zone_path = zone_name_path.at(1);
-	} else {
-		zone_path = zone;
-		zone_name = zone;
-		boost::replace_all(zone_name, "/etc/bind/db.", "");
-	}
-	//Check if given DNS Zone file exists
-	if(!(file_exists(zone_path))) {
-                std::cerr << "ERROR: The given DNS Zone file (" << zone_path << ") does not exist!" << std::endl;
-                return;
-        }
-	std::fstream f;
-	//Get actual serial + zone version
-	f.open("/etc/ddns/.serial_old.ddns", std::ios::out);
-	std::string serial_dig = shell_exec("dig +short @localhost "+zone_name+" SOA | awk '{print $3}'");
-	boost::replace_all(serial_dig, "\n", "");
-	boost::replace_all(serial_dig, "\r", "");
-        f << serial_dig;
-        f.close();
-        std::string date_dig = serial_dig.substr(0, serial_dig.length() - 2);
-	date_dig = date_dig + "0";
-        f.open("/etc/ddns/.date_old.ddns", std::ios::out);
-        f << date_dig;
-        f.close();
-	std::string version_dig = serial_dig;
-	boost::replace_all(version_dig, date_dig, "");
-	if(version_dig.substr(0, 1) == "0") {
-		boost::replace_all(version_dig, "0", "");
-	}
-	f.open("/etc/ddns/.version.ddns", std::ios::out);
-	f << version_dig;
-	f.close();
-	//Get last DNS Update
-	f.open("/etc/ddns/.date_old.ddns", std::fstream::in );
-	std::string date_old;
-	getline( f, date_old, '\0');
-	f.close();
-	//Write actual time
-	std::time_t tt = time(0);
-	f.open("/etc/ddns/.date.ddns", std::ios::out);
-	f << Time( tt, "%Y%m%d0" );
-	f.close();
-	//Read actual time
-	f.open("/etc/ddns/.date.ddns", std::fstream::in );
-	std::string date;
-	getline( f, date, '\0');
-	f.close();
-	//Check if DNS Zone already was updated at the same day
-	if (date_old != date) {
-		//If not set DNS Zone Version to 0
-		f.open("/etc/ddns/.date_old.ddns", std::ios::out);
-		f << Time( tt, "%Y%m%d0" );
-		f.close();
-		int zeit = 0;
-		f.open("/etc/ddns/.version.ddns", std::ios::out);
-		f << zeit;
-		f.close();
-   	}
-	//Read DNS Zone Version
-	f.open("/etc/ddns/.version.ddns", std::fstream::in );
-	std::string version_str;
-	getline( f, version_str, '\0');
-	f.close();
-	//Convert DNS Zone Version to Integer, add 1 and convert it back to string
-	int version_int = atoi(version_str.c_str());
-	version_int = version_int + 1;
-	std::stringstream versionss;
-	versionss << version_int;
-	std::string version = versionss.str();
-	f.open("/etc/ddns/.version.ddns", std::ios::out);
-	f << version;
-	f.close();
-	//Check if there were 10 or more updates at the same day
-	if (version_int >= 10) {
-		//If yes generate date without 0 as placeholder at the end
-		std::time_t tt = time(0);
-		f.open("/etc/ddns/.10-date.ddns", std::ios::out);
-		f << Time( tt, "%Y%m%d" );
-		f.close();
-		f.open("/etc/ddns/.10-date.ddns", std::fstream::in );
-		std::string date;
-		getline( f, date, '\0');
-		f.close();
-	}
-	//Add Version to date to create DNS Zone Serial
-	std::string serial = date + version;
-	//Read actual DNS Zone Serial
-	f.open("/etc/ddns/.serial_old.ddns", std::fstream::in );
-	std::string serial_old;
-	getline( f, serial_old, '\0');
-	f.close();
-	//Write new Serial to file
-	f.open("/etc/ddns/.serial_old.ddns", std::ios::out);
-	f << serial;
-	f.close();
-	//Write new IP to file
-	if(!ipv6) {
-		f.open("/etc/ddns/.oldip.ddns", std::ios::out);
-	} else {
-		f.open("/etc/ddns/.oldip6.ddns", std::ios::out);
-	}
-	f << IP;
-	f.close();
-	//Read DNS Zone file
-	f.open(zone_path, std::fstream::in );
-	std::string dnszone;
-	getline( f, dnszone, '\0');
-	f.close();
-	//Replace in DNS Zone: OLDIP with IP and serial_old with serial
-	std::string n = "\n";
-	boost::replace_all(IP, "\n", "");
-	boost::replace_all(IP, "\r", "");
-	boost::replace_all(OLDIP, "\n", "");
-	boost::replace_all(dnszone, OLDIP, IP);
-	boost::replace_all(serial_old, "\n", "");
-	boost::replace_all(serial, "\n", "");
-	boost::replace_all(dnszone, serial_old, serial);
-	//Write DNS Zone to file
-	f.open(zone_path, std::ios::out);
-	f << dnszone;
-	f.close();
-	//Run post update commands
-	//system(cmds);
-	std::cout << "DNS Zone " << zone_name <<  " was updated to Serial " << serial << " !" << std::endl;
-}
+#include "ddnsd.h"
 
 int main(int argc, char** argv) {
 	std::fstream f;
 	std::string version = "v5.1.4";
 	std::string release_date = "17.06.2018";
 	std::string config = "/etc/ddns/ddnsd.conf";
-	std::string update_checker = read_config(config, "update_checker = ");
+	std::string update_checker = utils::read_config(config, "update_checker = ");
 	if (update_checker == "true") {
-		std::string remote_version = shell_exec("curl --silent https://raw.githubusercontent.com/Schmorzel/ddnsd/master/.version");
+		std::string remote_version = utils::shell_exec("curl --silent https://raw.githubusercontent.com/Schmorzel/ddnsd/master/.version");
      	   	boost::replace_all(remote_version, "\n", "");
         	boost::replace_all(remote_version, "\r", "");
         	if (remote_version != version) {
@@ -251,25 +33,25 @@ int main(int argc, char** argv) {
 	}
 	//Read config
 	std::string enabled;
-	enabled = read_config(config, "enabled = ");
+	enabled = utils::read_config(config, "enabled = ");
 	if (enabled == "false") {
 		std::cout << "/etc/ddns/ddnsd.conf: enabled is set to false... Stopping service..." << std::endl;
 		exit(0);
 	}
 	std::string update_freq_string;
-	update_freq_string = read_config(config, "update_freq = ");
+	update_freq_string = utils::read_config(config, "update_freq = ");
 	std::string zones_string;
-	zones_string = read_config(config, "zones = ");
+	zones_string = utils::read_config(config, "zones = ");
 	std::vector<std::string> zones;
 	boost::split(zones, zones_string, boost::is_any_of(","));
 	zones_string.clear();
 	std::string cmds_string;
-	cmds_string = read_config(config, "post_update_cmds = ");
+	cmds_string = utils::read_config(config, "post_update_cmds = ");
 	std::vector<std::string> cmds;
 	boost::split(cmds, cmds_string, boost::is_any_of(","));
 	cmds_string.clear();
 	std::string config_version;
-	config_version = read_config(config, "config_version = ");
+	config_version = utils::read_config(config, "config_version = ");
 	f.open("/run/ddnsd.pid", std::ios::out );
 	f << getpid();
 	f << "\n";
@@ -306,17 +88,17 @@ int main(int argc, char** argv) {
 		getline(f, OLDIP6, '\0');
 		f.close();
 		//Get actual IP-Adress
-		std::string IP = shell_exec("curl --silent https://v4.ident.me/");
-		std::string IP6 = shell_exec("curl --silent https://v6.ident.me/");
+		std::string IP = utils::shell_exec("curl --silent https://v4.ident.me/");
+		std::string IP6 = utils::shell_exec("curl --silent https://v6.ident.me/");
 		//Check if IP is a valid IP-Adress
 		//e.g if no internet connection is available
 		bool ipv4 = true;
-		if (!is_ipv4_address(IP)) {
+		if (!ddnsd::is_ipv4_address(IP)) {
 			std::cerr << "ERROR: Failed to get valid IPv4-Address" << std::endl;
 			//Set IP to OLDIP to skip updating DNS Zone
 			IP = OLDIP;
 			ipv4 = false;
-		} if(!is_ipv6_address(IP6)) {
+		} if(!ddnsd::is_ipv6_address(IP6)) {
 			IP6 = OLDIP6;
 			if(!ipv4) {
 				std::cerr << "ERROR: Failed to get valid IPv6 Address" << std::endl;
@@ -325,12 +107,12 @@ int main(int argc, char** argv) {
 		if (IP != OLDIP || IP6 != OLDIP6) {
 			if(IP != OLDIP) {
 				for(std::string tmpStr : zones) {
-					updateip(tmpStr, OLDIP, IP, false);
+					ddnsd::updateip(tmpStr, OLDIP, IP, false);
 				}
 			}
 			if(IP6 != OLDIP6) {
 				for(std::string tmpStr : zones) {
-					updateip(tmpStr, OLDIP6, IP6, true);
+					ddnsd::updateip(tmpStr, OLDIP6, IP6, true);
 				}
 			}
 			for(std::string tmpStr : cmds) {
